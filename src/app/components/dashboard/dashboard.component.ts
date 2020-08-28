22689b5f2bc75e7eb707d3f69d71bf188e6a37e3
Andrii Tretiak <atretyak1985@gmail.com>
2020-08-28 23:34:21 +0300

    added functionality to sync text

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit, ViewChild, ElementRef, AfterViewInit} from '@angular/core';
 import { AuthService } from '../../shared/services/auth.service';
-
+import { take } from 'rxjs/operators';
 import MediumEditor from 'medium-editor';
 
 @Component({
@@ -11,16 +11,32 @@ import MediumEditor from 'medium-editor';
 export class DashboardComponent implements OnInit, AfterViewInit {
 
   constructor(
-    public authService: AuthService
+    public authService: AuthService,
   ) { }
 
   @ViewChild('medium') media: ElementRef;
   ngAfterViewInit() {
     const edit = this.media.nativeElement;
-    const editor = new MediumEditor(edit);
+    let editor;
 
-    editor.subscribe('editableInput', function (eventObj, editable) {
-      MathJax.Hub.Queue(['Typeset', MathJax.Hub, edit]);
+    let value;
+    let textCollection = this.authService.GetText().pipe(take(1)).subscribe(val => {
+      let placeholder = '';
+      if(!val || val.length == 0){
+        placeholder = 'Type some text';
+      }
+      edit.innerHTML = val;
+      const editor = new MediumEditor(edit, {
+        placeholder: {
+          text: placeholder
+        }
+      });
+
+      editor.subscribe('editableInput', (eventObj, editable) => {
+        MathJax.Hub.Queue(['Typeset', MathJax.Hub, edit]);
+        var content = editor.getContent();
+        this.authService.UpdateText(content);
+      });
     });
   }
 
--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -4,75 +4,36 @@ import { auth } from 'firebase/app';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
 import { Router } from '@angular/router';
+import { map,filter } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class AuthService {
-  userData: any; // Save logged in user data
+  _userData: any; // Save logged in user data
+
+  _db:AngularFirestore;
 
   constructor(
-    public afs: AngularFirestore,   // Inject Firestore service
+    public db: AngularFirestore,   // Inject Firestore service
     public afAuth: AngularFireAuth, // Inject Firebase auth service
     public router: Router,
     public ngZone: NgZone // NgZone service to remove outside scope warning
   ) {
+    this._db = db;
     /* Saving user data in localstorage when
     logged in and setting up null when logged out */
     this.afAuth.authState.subscribe(user => {
       if (user) {
-        this.userData = user;
-        localStorage.setItem('user', JSON.stringify(this.userData));
+        this._userData = user;
+        localStorage.setItem('user', JSON.stringify(this._userData));
         JSON.parse(localStorage.getItem('user'));
+        this.UserSigned();
       } else {
         localStorage.setItem('user', null);
         JSON.parse(localStorage.getItem('user'));
       }
-    });
-  }
-
-  // Sign in with email/password
-  SignIn(email, password) {
-    return this.afAuth.signInWithEmailAndPassword(email, password)
-      .then((result) => {
-        this.ngZone.run(() => {
-          this.router.navigate(['dashboard']);
-        });
-        this.SetUserData(result.user);
-      }).catch((error) => {
-        window.alert(error.message);
-      });
-  }
-
-  // Sign up with email/password
-  SignUp(email, password) {
-    return this.afAuth.createUserWithEmailAndPassword(email, password)
-      .then((result) => {
-        /* Call the SendVerificaitonMail() function when new user sign
-        up and returns promise */
-        this.SendVerificationMail();
-        this.SetUserData(result.user);
-      }).catch((error) => {
-        window.alert(error.message);
-      });
-  }
-
-  // Send email verfificaiton when new user sign up
-  SendVerificationMail() {
-    return this.afAuth.currentUser.then(u => u.sendEmailVerification())
-    .then(() => {
-      this.router.navigate(['verify-email-address']);
-    });
-  }
-
-  // Reset Forggot password
-  ForgotPassword(passwordResetEmail) {
-    return this.afAuth.sendPasswordResetEmail(passwordResetEmail)
-    .then(() => {
-      window.alert('Password reset email sent, check your inbox.');
-    }).catch((error) => {
-      window.alert(error);
     });
   }
 
@@ -91,12 +52,15 @@ export class AuthService {
   AuthLogin(provider) {
     return this.afAuth.signInWithPopup(provider)
     .then((result) => {
-       this.ngZone.run(() => {
-          this.router.navigate(['dashboard']);
-        });
       this.SetUserData(result.user);
     }).catch((error) => {
       window.alert(error);
+    });
+  }
+
+  UserSigned() {
+    this.ngZone.run(() => {
+      this.router.navigateByUrl('/dashboard');
     });
   }
 
@@ -104,15 +68,15 @@ export class AuthService {
   sign up with username/password and sign in with social auth
   provider in Firestore database using AngularFirestore + AngularFirestoreDocument service */
   SetUserData(user) {
-    const userRef: AngularFirestoreDocument<any> = this.afs.doc(`users/${user.uid}`);
-    const userData: User = {
+    const userRef: AngularFirestoreDocument<any> = this._db.doc(`users/${user.uid}`);
+    const _userData: User = {
       uid: user.uid,
       email: user.email,
       displayName: user.displayName,
       photoURL: user.photoURL,
       emailVerified: user.emailVerified
     };
-    return userRef.set(userData, {
+    return userRef.set(_userData, {
       merge: true
     });
   }
@@ -125,4 +89,33 @@ export class AuthService {
     });
   }
 
+  GetText(){
+    const user:User = JSON.parse(localStorage.getItem('user'));
+
+    return this._db
+      .collection('latex_text')
+      .snapshotChanges()
+      .pipe( map( actions =>
+        actions.filter( a =>
+        {
+          return a.payload.doc.id == user.uid
+        })
+          .map( a =>
+          {
+            const data:any = a.payload.doc.data();
+            const id = a.payload.doc.id;
+            return data && data.text
+          }
+        )
+      ));
+  }
+
+  UpdateText(text){
+    const user:User = JSON.parse(localStorage.getItem('user'));
+
+    this._db.collection('latex_text').doc(user.uid).set({
+      user_id: user.uid,
+      text: text,
+    });
+  }
 }
